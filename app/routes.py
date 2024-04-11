cfc050cd8d1a0ec7f99b7ebc83a4ccd35258b001
FiazMushfeq <fiazmushfeq@live.com>
2024-04-11 17:53:41 -0400

    Finished form

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     user = {'username': 'Admin'}
     posts = [
@@ -17,7 +17,6 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
-
 # '''
 # <html>
 #     <head>
@@ -27,3 +26,12 @@ def index():
 #         <h1>Hello, ''' + user['username'] + '''!</h1>
 #     </body>
 # </html>'''
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me()'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
